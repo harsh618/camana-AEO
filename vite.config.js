00ecfd4fb599db85fb191aef181b1e4413adf98c
User <user@example.com>
2026-02-03 01:28:24 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -27,6 +27,7 @@ export default defineConfig(async () => {
 
   return {
     logLevel: 'error', // Suppress warnings, only show errors
+    publicDir: 'public',
     resolve: {
       alias: {
         '@': resolve(__dirname, 'src')
